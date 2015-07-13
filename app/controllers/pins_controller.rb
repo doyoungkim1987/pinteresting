56ed370d22e02513e13664e489c867369168c50c
Do Young Kim <doyoungkim1987@gmail.com>
2015-07-12 18:55:28 -0700

    edited pins_controller .create

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -11,14 +11,14 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = current_user.pins.build
+    @pin = current_user.pins.new
   end
 
   def edit
   end
 
   def create
-    @pin = current_user.pins.build(pin_params)
+    @pin = current_user.pins.create(pin_params)
       if @pin.save
         redirect_to @pin, notice: 'Pin was successfully created.'
       else
